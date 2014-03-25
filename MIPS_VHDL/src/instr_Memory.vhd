0e0d649272b70b201a7b1a965f35694d992cb808
cambelltom <cambelltom@yahoo.com>
2014-03-25 21:39:41 +0100

    Update instr_Memory.vhd

--- MIPS_VHDL/src/instr_Memory.vhd
+++ MIPS_VHDL/src/instr_Memory.vhd
@@ -2,14 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity Instr_Memory is
-  port (    reset   : in std_logic;
-            rd      : in std_logic;
-            address : in std_logic_vector(4 downto 0);			
-			data    : inout std_logic_vector(7 downto 0)  );
-end entity Instr_Memory;
+  GENERIC (N:     NATURAL);  -- Width of inputs.
+  port (address : in std_logic_vector(N-1 downto 0);			
+		data    : inout std_logic_vector(N-1 downto 0));
+end Instr_Memory;
 
 architecture behavioral of Instr_Memory is
-  type mem is array ( 0 to 2**2 - 1) of std_logic_vector(7 downto 0);
+  type mem is array ( 0 to 2**2 - 1) of std_logic_vector(N-1 downto 0);
   
   constant my_Rom : mem := (
     0  => "11100001",
@@ -18,9 +17,8 @@ architecture behavioral of Instr_Memory is
     3  => "11101000");
 	 
 begin
-   process(address,rd) 
+   process(address) 
 	begin
-	IF rd='1' THEN
      case address is
        when "00000" => data <= my_rom(0);
        when "00001" => data <= my_rom(1);
@@ -28,8 +26,5 @@ begin
        when "00011" => data <= my_rom(3); 
        when others => data <= "00000000";
 	 end case;
-	ELSE
-		data<="ZZZZZZZZ";
-	END IF;
   end process;
 end architecture behavioral;
