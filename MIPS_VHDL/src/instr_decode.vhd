296a9b8c4ff84f751b7b66499b0ed85ca7583c59
Harr1l <harri.laine@gmail.com>
2014-04-08 16:42:53 +0200

    Update instr_decode.vhd - compiles on modelsim

--- MIPS_VHDL/src/instr_decode.vhd
+++ MIPS_VHDL/src/instr_decode.vhd
@@ -11,6 +11,7 @@ use IEEE.numeric_std.all;
 use IEEE.STD_LOGIC_ARITH.all;
 
 entity instr_decode is
+GENERIC (MIPS_SIZE: NATURAL:= 32; ADDR_SIZE: NATURAL:= 8);
 	port(
 		clk : in std_logic;
 		rst : in std_logic;
@@ -22,13 +23,13 @@ entity instr_decode is
 		jmp_addr : out std_logic_vector(25 downto 0);
 		pc_addr_in : in std_logic_vector(MIPS_SIZE-1 downto 0);
 		pc_addr_out : out std_logic_vector(MIPS_SIZE-1 downto 0);
-		sign_extend = out std_logic_vector(7 downto 0);
+		sign_extend : out std_logic_vector(7 downto 0);
 		-- alu_ctrl = 010 -> ADD, 110 -> SUB, 000 -> AND, 001 -> OR, 111 -> SLT, 100 -> MULT, 011 -> DIV, 101 -> SHIFT
-		alu_ctrl : out std_logic_vector(2 downto 0);
-	)
+		alu_ctrl : out std_logic_vector(2 downto 0)
+	);
 end instr_decode;
 
-architecture behavior of instr_decode is
+architecture behaviour of instr_decode is
 	signal reg_1_p, reg_2_p, reg_3_p : std_logic_vector(ADDR_SIZE-1 downto 0);
 	signal imm_p : std_logic_vector(MIPS_SIZE-1 downto 0);
 	signal jmp_addr_p : std_logic_vector(25 downto 0);
@@ -44,10 +45,6 @@ component register_file is
 		r2_addr	: in std_logic_vector(ADDR_SIZE-1 downto 0);
 		r3_addr	: in std_logic_vector(ADDR_SIZE-1 downto 0);
 		wr_data : in std_logic_vector(MIPS_SIZE-1 downto 0);
-		--signals from WB stage
-		rw : in std_logic;
-		wr_data : in std_logic_vector(MIPS_SIZE-1 downto 0);
-		--
 		
 		reg_2 : out std_logic_vector(MIPS_SIZE-1 downto 0);
 		reg_3 : out std_logic_vector(MIPS_SIZE-1 downto 0)
@@ -60,16 +57,17 @@ register_file_i : register_file
 port map(
 		clk => clk,
 		rst => rst,
-		rw  => rw,
+		rw  => '0',
 		r1_addr	=> reg_1_p,
 		r2_addr	=> reg_2_p,
-		r3_addr	=> reg_3_p
-		wr_data => wr_data,
-		reg_2 => reg_2, 
-		reg_3 => reg_3
+		r3_addr	=> reg_3_p,
+		wr_data => (others => '0'),
+		reg_2 => open, 
+		reg_3 => open
 	);
 	
-	
+	process(clk)
+	begin
 	case instr(31 downto 26) is
 		when "000000" => --register functions
 			-- read register 2 and 3 and get register write address
@@ -99,32 +97,34 @@ port map(
 			end case;
 		when "000001" => --jmp
 			jmp_addr <= instr(25 downto 0);
-			case jmp_addr(25) is
-				when "1" =>
-					sign_extend_p <= "1" & instr(6 downto 0);
+			case instr(25) is
+				when '1' =>
+					sign_extend_p <= '1' & instr(6 downto 0);
 				when others =>
-					sign_extend_p <= "0" & instr(6 downto 0);
+					sign_extend_p <= '0' & instr(6 downto 0);
 			end case;
 		when others => --immediate and rest
+			reg_1_p <= instr(25 downto 21);
+			reg_2_p <= instr(20 downto 16); --from r2
+			imm_p(15 downto 0) <= instr(15 downto 0); --from imm (we want to user 32-bit values)
+			imm_p(MIPS_SIZE-1 downto 16) <= (others => '0');
 			case instr(31 downto 26) is
-				reg_1_p <= instr(25 downto 21);
-				reg_2_p <= instr(20 downto 16); --from r2
-				imm_p(15 downto 0) <= instr(15 downto 0); --from imm (we want to user 32-bit values)
-				imm_p(MIPS_SIZE-1 downto 16) <= (others => '0');
 				when "100001" => --addi
 					alu_ctrl_p <= "010";
 				when "000010" => --beq
 					case instr(15) is
-						when "1" =>
-							sign_extend_p <= "1" & instr(6 downto 0);
+						when '1' =>
+							sign_extend_p <= '1' & instr(6 downto 0);
 						when others =>
-							sign_extend_p <= "0" & instr(6 downto 0);
+							sign_extend_p <= '0' & instr(6 downto 0);
 					end case;
 				when "100000" => --lb
 				when "110000" => --sb
 				when others =>
 			end case;
-	end case
+	end case;
+	end process;
+	
 	process(clk,rst)
 	begin
 		if rst='1' then
@@ -143,5 +143,4 @@ port map(
 			sign_extend <= sign_extend_p;
 		end if;
 	end process;
-end behavior
-		
+end behaviour;
