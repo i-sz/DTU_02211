5bebee852c02ae663ce261240bace1b25a7fe0f9
cambelltom <cambelltom@yahoo.com>
2014-04-22 14:16:40 +0200

    Update alu.vhd

--- MIPS_VHDL/src/alu.vhd
+++ MIPS_VHDL/src/alu.vhd
@@ -1,33 +1,37 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;		
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_signed.ALL;
 use IEEE.numeric_std.all;
 
 entity alu is
 port(
-	input_a : in std_logic_vector(7 downto 0);
-	input_b : in std_logic_vector(7 downto 0);
+	input_a : in std_logic_vector(31 downto 0);
+	input_b : in std_logic_vector(31 downto 0);
 	ctrl	: in std_logic_vector(2 downto 0);
-	output  : out std_logic_vector(7 downto 0)
+	output  : out std_logic_vector(31 downto 0)
 );
 end alu;
-
 architecture behaviour of alu is
+signal output64  : std_logic_vector(63 downto 0);
 
 begin
 
 process(input_a, input_b, ctrl)
 begin
 	case ctrl is
-		when "000" => output <= input_a and input_b;
-		when "001" => output <= input_a or input_b;
-		when "010" => output <= input_a + input_b;
-		when "011" => output <= input_a - input_b;
-		when "100" => output <= (others => '0');
-		when "101" => output <= (others => '0');
-		when "110" => output <= (others => '0');
-		when "111" => output <= (others => '0');
+		when "000" => output <= std_logic_vector(unsigned(input_a) and unsigned(input_b));
+		when "001" => output <= std_logic_vector(unsigned(input_a) or unsigned(input_b));
+		when "010" => output <= std_logic_vector(unsigned(input_a) + unsigned(input_b));
+		when "011" => output <= std_logic_vector(unsigned(input_a) / unsigned(input_b));
+		when "100" => output64 <= std_logic_vector(unsigned(input_a) * unsigned(input_b));
+		               output <= output64(31 downto 0);
+		when "101" => output <= std_logic_vector(unsigned(input_a) sll to_integer(unsigned(input_b)));
+		when "110" => output <= std_logic_vector(unsigned(input_a) - unsigned(input_b));
+		when "111" => 
+		 if (unsigned(input_a) < unsigned(input_b)) then
+		  output <= (others => '1');
+		  else 
+		  output <=(others => '0'); 
+		 end if;  
 		when others => output <= (others => '0');
  	end case;
 end process;
