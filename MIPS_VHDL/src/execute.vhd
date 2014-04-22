911aa2bf415b5328495f358b6c9c2699c54bbb90
cambelltom <cambelltom@yahoo.com>
2014-04-22 14:18:37 +0200

    Update execute.vhd

--- MIPS_VHDL/src/execute.vhd
+++ MIPS_VHDL/src/execute.vhd
@@ -7,22 +7,17 @@ entity  execute is
 port(
 	clk : in std_logic;
 	rst : in std_logic; 
-	a   : in std_logic_vector(7 downto 0);
-	b   : in std_logic_vector(7 downto 0);
-	sign_extend : in std_logic_vector(7 downto 0);
+	a   : in std_logic_vector(31 downto 0);
+	b   : in std_logic_vector(31 downto 0);
+	sign_extend : in std_logic_vector(31 downto 0);
 	alu_ctrl : in std_logic_vector(2 downto 0);
 	alu_src : in  std_logic;
 	pc_addr_in : in std_logic_vector(31 downto 0);
-	
-	alu_result : out std_logic_vector(7 downto 0);
-	
+	alu_result : out std_logic_vector(31 downto 0);
 	ctrl : out std_logic_vector(2 downto 0);
-	
 	jump_instr : in std_logic_vector(7 downto 0);
 	jump_addr : out std_logic_vector(7 downto 0);
-
 	branch_out : out std_logic_vector(7 downto 0)
-	
 );
 
 end execute;
@@ -30,26 +25,21 @@ end execute;
 
 architecture behaviour of execute is
 
-signal input_a, input_b, alu_output : std_logic_vector(7 downto 0);
---signal alu_ctrl : std_logic_vector(2 downto 0); 
-
+signal input_a, input_b : std_logic_vector(31 downto 0);
 signal branch_add, jump_add, jump_addr_p : std_logic_vector(7 downto 0);
 --signals for the pipeline stage
-
-signal alu_result_p : std_logic_vector(7 downto 0);
+signal alu_result_p : std_logic_vector(31 downto 0);
 signal ctrl_p : std_logic_vector(2 downto 0);
-
 signal branch_out_p : std_logic_vector(7 downto 0);
-
 
 
 
 component alu is
 port(
-	input_a : in std_logic_vector(7 downto 0);
-	input_b : in std_logic_vector(7 downto 0);
+	input_a : in std_logic_vector(31 downto 0);
+	input_b : in std_logic_vector(31 downto 0);
 	ctrl	: in std_logic_vector(2 downto 0);
-	output  : in std_logic_vector(7 downto 0)
+	output  : out std_logic_vector(31 downto 0)
 );
 
 end component;
@@ -72,7 +62,7 @@ input_b <= B when (alu_src ='0') else sign_extend;
 ctrl_p <= alu_ctrl;
 
 --Branching
-branch_add <= std_logic_vector(unsigned(pc_addr_in(7 DOWNTO 2)) + unsigned(sign_extend));
+branch_add <= std_logic_vector(unsigned(pc_addr_in(7 DOWNTO 2)) + unsigned(sign_extend(7 DOWNTO 0)));
 branch_out_p <= branch_add (7 DOWNTO 0); 
  
  --Jumping
