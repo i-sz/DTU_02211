f65f26503d9ba86c58402cdf620da88abfd993fc
Harr1l <harri.laine@gmail.com>
2014-05-01 14:19:49 +0200

    added multiplication test

--- maths.asm
+++ maths.asm
@@ -10,9 +10,20 @@
 # Simple additions and subtractions
 #
 
-
+; Simple + and - actions
 addi r01,r00,1
 addi r02,r00,2
-addi r31,r00,4
+addi r03,r00,3
+addi r04,r00,4
+add r31,r00,r03
 add r31,r31,r01
 sub r31,r31,r02
+
+
+; Multiplying
+addi r01,r00,1
+addi r02,r00,2
+addi r03,r00,3
+addi r04,r00,4
+mult r31,r02,r02
+mult r31,r02,r03
