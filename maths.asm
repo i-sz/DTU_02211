c60ac1290cca1afef682017a4fef7681b196b500
Harr1l <harri.laine@gmail.com>
2014-05-01 13:33:13 +0200

    addi, add, sub tests

---
+++ maths.asm
@@ -0,0 +1,18 @@
+#
+# for DTU course 02211
+#
+# Project by:
+# Harri Antero Laine: s131196
+# Istvan Szonyi: s131153
+# Komlan Tom Evon: s072728
+#
+#
+# Simple additions and subtractions
+#
+
+
+addi r01,r00,1
+addi r02,r00,2
+addi r31,r00,4
+add r31,r31,r01
+sub r31,r31,r02
