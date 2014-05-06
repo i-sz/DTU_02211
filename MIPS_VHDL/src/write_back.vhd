c58d89af84ba7465fa1b0b1cdc4a8ae9d3ae4244
Istvan Szonyi <pisti.szonyi@gmail.com>
2014-05-06 22:13:52 +0200

    Update write_back.vhd

--- MIPS_VHDL/src/write_back.vhd
+++ MIPS_VHDL/src/write_back.vhd
@@ -13,10 +13,12 @@ port(
 	wr_reg_in  : in std_logic_vector(ADDR_SIZE-1 downto 0);
 	wr_reg_out : out std_logic_vector(ADDR_SIZE-1 downto 0);
 	wr_flag    : out std_logic;
-	wr_data : out std_logic_vector(MIPS_SIZE-1 downto 0);
+	wr_data   : out std_logic_vector(MIPS_SIZE-1 downto 0);
 	wr_mem_wb : in std_logic;
 	rd_mem_wb : in std_logic;
-	branch : in std_logic
+	branch : in std_logic;
+	wb_reg_i : in std_logic
+
 );
 end write_back;
 
@@ -27,18 +29,22 @@ BEGIN
 
 process (clk, rst, alu_result, wr_reg_in, rd_data, wr_mem_wb, rd_mem_wb )
 	begin
-	 if (rst = '1' or wr_mem_wb = '1' or branch='1') then
+	 if (rst = '1') then
 	   wr_reg_out <= (others => '0');
 	   wr_data    <= (others => '0');
 	   wr_flag    <= '0';
 	elsif clk'event and clk = '1' then
 	   wr_reg_out <= wr_reg_in;
-		if (rd_mem_wb = '1') then
-			wr_data <= rd_data;
-		else
-			wr_data    <= alu_result;
+		if (rd_mem_wb = '1' and wr_mem_wb = '0'and wb_reg_i = '1') then
+			wr_data    <= rd_data;
+			wr_flag    <= '1';
+		elsif(rd_mem_wb = '0' and wr_mem_wb = '0' and wb_reg_i = '1') then
+			wr_data <= alu_result;
+			wr_flag    <= '1';
+		elsif(wb_reg_i ='0') then
+			wr_flag    <= '0';				
 		end if;
-       wr_flag    <= '1';	   
+       	   
 	end if;
   end process;
 
