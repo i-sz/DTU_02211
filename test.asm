df1092ab95c57a914839f95613c2a4d28c918d2d
Harr1l <harri.laine@gmail.com>
2014-05-07 18:10:58 +0200

    a

--- test.asm
+++ test.asm
@@ -12,24 +12,85 @@
 
 ; Simple + and - actions
 addi r01,r00,1
+nop
+nop
+nop
+nop
+nop
 addi r02,r01,2
+nop
+nop
+nop
+nop
+nop
 addi r03,r02,3
+nop
+nop
+nop
+nop
+nop
 addi r04,r03,4
+nop
+nop
+nop
+nop
+nop
 add r31,r01,r03
+nop
+nop
+nop
+nop
+nop
 add r30,r31,r01
+nop
+nop
+nop
+nop
+nop
 sub r31,r31,r02
+nop
+nop
+nop
+nop
+nop
 sb r01,r00,1
 sb r02,r00,2
 
+
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
 
 ; Multiplying
 addi r01,r00,1
 addi r02,r00,2
 addi r03,r00,3
 addi r04,r00,4
-mult r31,r02,r02
-mult r31,r02,r03
+nop
+nop
+nop
+nop
+nop
 
+mult r31,r02,r02
+nop
+nop
+nop
+nop
+nop
+mult r31,r02,r03
+nop
+nop
+nop
+nop
+nop
 lb r01,r00,1
 lb r02,r00,2
 
