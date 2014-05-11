4628a58d662c8d8795e6f5b7340f50afc2abdffc
Istvan Szonyi <pisti.szonyi@gmail.com>
2014-05-11 17:19:01 +0200

    Update uart_adapte_simpler.vhd

--- MIPS_VHDL/src/uart_adapte_simpler.vhd
+++ MIPS_VHDL/src/uart_adapte_simpler.vhd
@@ -35,9 +35,10 @@ architecture behav of uart_adapter is
 	
     uart_wr_ena <= mem_wr_ena;
     uart_rd_ena <= mem_rd_ena;
-    uart_addr <=  "01" when (mem_address(1 downto 0) = "00") else "00" ;  --when we write to uart
+    uart_addr <=  "01" when (mem_address(1 downto 0) = "00") else 
+						"00" when (mem_address(3 downto 0) >= "0001") and (mem_address(4 downto 0) < "10001") else "00";    --when we write to uart
     uart_wr_data <= mem_data_in;
-	to_proc_data_in <= uart_rd_data;
+	 to_proc_data_in <= uart_rd_data;
     uart_rdy_cnt_s <= uart_rdy_cnt;
-    to_proc_rd_ena <= '1' when (mem_address(1 downto 0) = "01") else '0';
+    to_proc_rd_ena <= '1' when ((mem_address(3 downto 0) >= "0001") and (mem_address(4 downto 0) < "10001")) else '0';
 end behav;
